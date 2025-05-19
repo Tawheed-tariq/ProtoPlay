136902c371f5ea22210bcca34537fe43d3f54f52
ya_sonia <soniayadavnitsgr@gmail.com>
2025-05-19 20:21:43 +0530

    RIP-Protocol

---
+++ pyRIP/README.md
@@ -0,0 +1,25 @@
+# pyRIP
+![Platform](https://img.shields.io/badge/Platform-Windows-lightgrey.svg) ![Python](https://img.shields.io/badge/Python-3.5.1-blue.svg) ![Status](https://img.shields.io/badge/Status-Abandoned-cccccc.svg)
+
+[pyRIP](https://github.com/hankofficer/pyRIP) is a personal Routing Information Protocol Implementation on Python. In order to get familiar with implementing a routing protocol, I opened this project on GitHub for the practice.
+
+Currently, pyRIP is capable of originating routes and redistribute them to neighbor RIP router. This could be an easy tool to test or debug a RIP router.
+
+## Available functions
+1. Regular update packets
+2. Routing Information Base
+3. JSON configuration file
+4. Handling incoming packets
+
+## GOAL
+Implement RFC2453(RIPv2) and compatible with RFC1058(RIPv1)
+
+## TODO
+1. Split-Horizon and Poison Reverse
+2. Trigger update
+3. Compatible with v1
+4. RIPv2 Authentication
+
+## Reference
+* **RIPv2** [RFC 2453](http://tools.ietf.org/html/rfc2453)
+* **RIPv1** [RFC 1058](http://tools.ietf.org/html/rfc1058)
---
+++ pyRIP/pyrip.py
@@ -0,0 +1,432 @@
+#!/usr/bin/env python
+import sys
+import getopt
+import struct
+import time
+import socket
+import random
+import json
+
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+from pyrip_lib import *
+
+'''
+    Provides methods to handle RIP routes
+'''
+class IRoute(object):
+    def __init__(self, prefix=0, prefixLen=0, nextHop=0, afi=0):
+        self.afi = afi
+        self.prefix = prefix
+        self.prefixLen = prefixLen
+        self.nextHop = nextHop
+
+    def __cmp__(self, other):
+        if self.afi < other.afi:
+            return -1
+        elif self.afi > other.afi:
+            return 1
+
+        if self.prefix < other.prefix:
+            return -1
+        elif self.prefix > other.prefix:
+            return 1
+
+        if self.prefixLen < other.prefixLen:
+            return -1
+        elif self.prefixLen > other.prefixLen:
+            return 1
+
+        if self.nextHop < other.nextHop:
+            return -1
+        elif self.nextHop > other.nextHop:
+            return 1
+
+        return 0
+
+    def __lt__(self, other):
+        return self.__cmp__(other)<0
+    def __le__(self, other):
+        return self.__cmp__(other)<=0
+    def __eq__(self, other):
+        return self.__cmp__(other)==0
+    def __ne__(self, other):
+        return self.__cmp__(other)!=0
+    def __gt__(self, other):
+        return self.__cmp__(other)>0
+    def __ge__(self, other):
+        return self.__cmp__(other)>=0
+
+class RipRoute(IRoute):
+    def __init__(self, prefix, prefixLen, nextHop, metric=RIP_METRIC_MIN, routeTag=0, family=RIP_ADDRESS_FAMILY):
+        self.family = family
+        self.routeTag = routeTag
+        self.prefix = prefix
+        self.prefixLen = prefixLen
+        self.nextHop = nextHop
+        self.afi = 1 # IPv4
+        
+        if metric >= RIP_METRIC_INFINITY:
+            self.metric = RIP_METRIC_INFINITY
+        elif metric <= RIP_METRIC_MIN:
+            self.metric = RIP_METRIC_MIN
+        else:
+            self.metric = metric
+
+    def pack(self):
+        mask = PrefixLen2MaskInt(self.prefixLen)
+        return struct.pack(RIP_ENTRY_PACK_FORMAT,
+            self.family, self.routeTag, self.prefix, mask, self.nextHop, self.metric)
+
+    def __str__(self):
+        return '{'+'{:s}/{:d}, {:s}, {:d}, {:d}'.format(Int2IP(self.prefix), self.prefixLen, Int2IP(self.nextHop), self.metric, self.routeTag)+'}'
+
+    def __repr__(self):
+        return '{:08x}/{:d}->{:08x} m{:d} t{:d}'.format(self.prefix, self.prefixLen, self.nextHop, self.metric, self.routeTag)
+
+'''
+    Provides methods to handle RIP packets
+'''
+class RipPacket(object):
+    def __init__(self, cmd = RIP_COMMAND_RESPONSE, version = 2):
+        self.command = cmd
+        self.version = version
+        self.entry = []
+
+    @property
+    def size(self):
+        return RIP_HEADER_SIZE + len(self.entry)*RIP_ENTRY_SIZE
+
+    def pack(self):
+        header = struct.pack(RIP_HEADER_PACK_FORMAT,self.command,self.version,0)
+        entries = b''
+        for ent in self.entry:
+            entries = entries + ent.pack()
+        packet = header + entries
+        return packet
+
+    def unpack(data):
+        pkt = RipPacket()
+        hdr = data[:RIP_HEADER_SIZE]
+        data = data[RIP_HEADER_SIZE:]
+        pkt.command, pkt.version, zero = struct.unpack(RIP_HEADER_PACK_FORMAT, hdr)
+        while len(data) > 0 and len(data)%RIP_ENTRY_SIZE == 0:
+            ent = data[:RIP_ENTRY_SIZE]
+            data = data[RIP_ENTRY_SIZE:]
+            family, tag, prefix, mask, nextHop, metric = struct.unpack(RIP_ENTRY_PACK_FORMAT, ent)
+            pkt.entry.append(RipRoute(
+                prefix, MaskInt2PrefixLen(mask), nextHop, metric, tag, family))
+        return pkt
+
+    def addEntry(self, prefix, prefixLen, nextHop, metric, routeTag=0, family=RIP_ADDRESS_FAMILY):
+        self.entry.append(RipRoute(prefix, prefixLen, nextHop, metric, routeTag, family))
+        return True
+
+    def removeEntry(self, prefix, prefixLen, nextHop):
+        for ent in self.entry:
+            if ent.prefix == prefix and ent.prefixLen == prefixLen and ent.nextHop == nextHop:
+                self.entry.remove(ent)
+                return True
+        return False
+
+    def __getitem__(self, key):
+        return self.entry[key]
+
+    def __setitem__(self, key, value):
+        self.entry[key] = value
+
+    def __repr__(self):
+        if self.command == RIP_COMMAND_REQUEST:
+            cmd = 'request'
+        elif self.command == RIP_COMMAND_RESPONSE:
+            cmd = 'response'
+
+        ret = 'v{:d}, {:s}, {:d} entries'.format(self.version,cmd,len(self.entry))
+        return ret
+
+'''
+    main RIP process. RIP version 2.
+    version 1 should be added in future.
+'''
+class RIP(DatagramProtocol):
+    def __init__(self, inputDict):
+        self.ttl = 1 # link-local. Not going to follow history ttl=2.
+        self.updateTime = RIP_DEFAULT_UPDATE
+        self.timeoutTime = RIP_DEFAULT_TIMEOUT
+        self.garbageTime = RIP_DEFAULT_GARBAGE
+        self.jitterScale = RIP_DEFAULT_JITTER_SCALE
+        self.RIB = []
+        self.loadConfigurationFile(inputDict['configFileName'])
+
+    ''' Twisted Functions '''
+    def startProtocol(self):
+        self.transport.setTTL(self.ttl)
+        self.transport.joinGroup(RIP_MULTICAST_ADDR)
+        self.transport.setLoopbackMode(False)
+        self.transport.setBroadcastAllowed(True)
+        #
+        self.requestAllRoutes()
+        reactor.callLater(self.getUpdateTime(), self.sendRegularUpdate)
+
+    def datagramReceived(self, datagram, addrPort):
+        pkt = RipPacket.unpack(datagram)
+        print('r', pkt, repr(addrPort))
+
+        # we only deal with RIPv2 now.
+        if pkt.version != 2:
+            return
+
+        if pkt.command == RIP_COMMAND_REQUEST:
+            self.respondToRequest(pkt.entry, addrPort[0])
+        elif pkt.command == RIP_COMMAND_RESPONSE: 
+            self.updateRIB(pkt.entry, addrPort[0])
+
+    def connectionRefused(self):
+        pass
+
+    ''' RIP Functions '''
+    def loadConfigurationFile(self, filename):
+        with open(filename, 'r') as f:
+            conf = json.load(f)
+
+        if 'updateTime' in conf:
+            self.updateTime = conf['updateTime']
+        if 'timeoutTime' in conf:
+            self.timeoutTime = conf['timeoutTime']
+        if 'garbageTime' in conf:
+            self.garbageTime = conf['garbageTime']
+
+        if 'routes' in conf:
+            for r in conf['routes']:
+                if set(('prefix', 'prefixLen', 'nextHop')) <= set(r.keys()):
+                    prefix = IP2Int(r['prefix'])
+                    prefixLen = r['prefixLen']
+                    nextHop = IP2Int(r['nextHop'])
+                    metric = RIP_METRIC_MIN if 'metric' not in r.keys() else r['metric']
+                    rtag = 0 if 'routeTag' not in r.keys() else r['routeTag']
+                    self.addRouteToRIB(RipRoute(prefix, prefixLen, nextHop, metric, rtag))
+
+    def getUpdateTime(self):
+        return self.updateTime + (random.random()*2-1)*self.updateTime*self.jitterScale
+
+    # TODO
+    def respondToRequest(self, routes, addr):
+        pkt = RipPacket(RIP_COMMAND_RESPONSE, 2)
+        for r in routes:
+            if r.prefix == 0 and r.prefixLen == 0:
+                for rib_entry in self.RIB:
+                    pkt.addEntry(rib_entry.prefix, rib_entry.prefixLen, rib_entry.nextHop, rib_entry.metric, rib_entry.routeTag)
+            else:
+                for rib_entry in self.RIB:
+                    if rib_entry.prefix == r.prefix and rib_entry.prefixLen == r.prefixLen:
+                        pkt.addEntry(rib_entry.prefix, rib_entry.prefixLen, rib_entry.nextHop, rib_entry.metric, rib_entry.routeTag)
+        if pkt.entry:
+            print('s [Response]', pkt)
+            self.transport.write(pkt.pack(), (addr, RIP_UDP_PORT))
+
+    def updateRIB(self, routes, addr):
+        for r in routes:
+            if r.nextHop == 0:
+                r.nextHop = IP2Int(addr)
+            self.addOrUpdateRouteInRIB(r)
+        self.refreshRIB()
+        self.showRIB()   # optional, shows updated RIB
+        
+
+    def addOrUpdateRouteInRIB(self, route):
+    # Check if route for prefix/prefixLen exists
+        for i, r in enumerate(self.RIB):
+            if r.prefix == route.prefix and r.prefixLen == route.prefixLen:
+                # Update only if new metric is better or nextHop differs
+                if route.metric < r.metric or route.nextHop != r.nextHop:
+                    self.RIB[i] = route
+                return
+        # If not found, add new route
+        self.RIB.append(route)
+        self.RIB.sort()  # optional, keeps RIB sorted
+
+
+    def sendRegularUpdate(self):
+        pkt = RipPacket(RIP_COMMAND_RESPONSE, 2)
+        for r in self.RIB:
+            if r.metric < RIP_METRIC_MAX:
+                pkt.addEntry(r.prefix, r.prefixLen, r.nextHop, r.metric)
+
+        if len(pkt.entry) > 0:
+            print('s', pkt, pkt.size)
+            self.transport.write(pkt.pack(), (RIP_MULTICAST_ADDR, RIP_UDP_PORT))
+        else:
+            print('s No valid entry to send update packet.')
+
+        reactor.callLater(self.getUpdateTime(), self.sendRegularUpdate)
+
+    # TODO
+    def sendRequest(self, route):
+        pkt = RipPacket(RIP_COMMAND_REQUEST, 2)
+        pkt.addEntry(route.prefix, route.prefixLen, 0, RIP_METRIC_INFINITY)
+        print('s [Request specific route]', pkt)
+        self.transport.write(pkt.pack(), (RIP_MULTICAST_ADDR, RIP_UDP_PORT))
+
+    def requestAllRoutes(self):
+        pkt = RipPacket(RIP_COMMAND_REQUEST, 2)
+        pkt.addEntry(0, 0, 0, RIP_METRIC_INFINITY, 0, 0)
+        print('s [Request all routes]', pkt)
+        self.transport.write(pkt.pack(), (RIP_MULTICAST_ADDR, RIP_UDP_PORT))
+
+    '''
+        RIB database functions
+    '''
+    def routeTimerSetup(self, route):
+        if route.metric == RIP_METRIC_INFINITY:
+            self.garbageRoute(route)
+        else:
+            route.timeoutTimer = reactor.callLater(self.timeoutTime, self.garbageRoute, route)
+            route.garbageTimer = None
+
+    def routeTimerReset(self, route):
+        if route.timeoutTimer:
+            route.timeoutTimer.reset(self.timeoutTime)
+        else:
+            route.timeoutTimer = reactor.callLater(self.timeoutTime, self.garbageRoute, route)
+            route.garbageTimer.cancel()
+            route.garbageTimer = None
+
+    def routeTimerTimeoutGet(self, route):
+        if route.timeoutTimer:
+            return int(route.timeoutTimer.getTime()-time.time())
+        return -1
+
+    def routeTimerGarbageGet(self, route):
+        if route.garbageTimer:
+            return int(route.garbageTimer.getTime()-time.time())
+        return -1
+
+    def garbageRoute(self, route):
+        route.timeoutTimer = None
+        route.metric = RIP_METRIC_INFINITY
+        route.garbageTimer = reactor.callLater(self.garbageTime, self.deleteRouteFromRIB, route)
+
+    def verifyRoute(self, route):
+        # check netmask
+        # check prefix
+        # check next hop
+
+        if route.metric < RIP_METRIC_MIN:
+            return False
+
+        if not route.family == RIP_ADDRESS_FAMILY:
+            return False
+
+        return True
+
+    def showRIB(self):
+        # Print header
+        print("\n--- Routing Table ---")
+        print(f"{'Destination':<15} {'Next Hop':<15} {'Metric':<6} {'Timeout':<8} {'Garbage':<8}")
+        print('-'*60)
+
+        for r in self.RIB:
+            dest_ip = Int2IP(r.prefix)
+            next_hop_ip = Int2IP(r.nextHop)
+            metric = r.metric
+            timeout = self.routeTimerTimeoutGet(r)
+            garbage = self.routeTimerGarbageGet(r)
+
+            print(f"{dest_ip:<15} {next_hop_ip:<15} {metric:<6} {timeout:<8} {garbage:<8}")
+            
+    # Find best route for given IP (Longest Prefix Match + lowest metric)
+    def longest_prefix_match(self, ip):
+        ip_int = IP2Int(ip)
+        best_route = None
+        best_prefix_len = -1
+        for route in self.RIB:
+            mask = PrefixLen2MaskInt(route.prefixLen)
+            if (ip_int & mask) == (route.prefix & mask):
+                if route.prefixLen > best_prefix_len:
+                    best_prefix_len = route.prefixLen
+                    best_route = route
+                elif route.prefixLen == best_prefix_len:
+                    if best_route and route.metric < best_route.metric:
+                        best_route = route
+        return best_route
+
+
+    def addRouteToRIB(self, route):
+        if self.verifyRoute(route) == False:
+            return
+
+        for r in self.RIB:
+            if r == route:
+                return
+
+        self.routeTimerSetup(route)
+
+        for i in range(0,len(self.RIB)):
+            if self.RIB[i] >= route:
+                self.RIB.insert(i, route)
+                return
+        self.RIB.append(route)
+        self.showRIB()
+
+    def deleteRouteFromRIB(self, route):
+        if len(self.RIB) == 0:
+            warn('Nothing to delete in RIB.')
+            return
+
+        for r in self.RIB:
+            if r == route:
+                self.RIB.remove(r)
+
+    def refreshRIB(self):
+        for r in self.RIB:
+            if r.metric >= RIP_METRIC_INFINITY:
+                r.metric = RIP_METRIC_INFINITY
+
+    def lookupRoute(self, ip):
+        route = self.longest_prefix_match(ip)
+        if route:
+            print(f"Best route for {ip} is {route}")
+        else:
+            print(f"No route found for {ip}")
+        return route
+
+'''
+    Show available options and details about this program.
+'''
+def showHelp():
+    print('pyrip.py <configFile>')
+
+'''
+    Parse input arguments into dictionary.
+'''
+def inputParser(argv):
+    inputDict = {}
+
+    try:
+        opts, args = getopt.getopt(argv,'h',['help'])
+    except getopt.GetoptError:
+        showHelp()
+        sys.exit()
+
+    for opt, arg in opts:
+        if opt == '-h':
+            showHelp()
+            sys.exit()
+
+    if len(args) == 0:
+        showHelp()
+        sys.exit()
+
+    inputDict['configFileName'] = args[0]
+    return inputDict
+
+def main(argv):
+    reactor.listenMulticast(RIP_UDP_PORT, RIP(inputParser(argv)), listenMultiple=True)
+    reactor.run()
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+    
